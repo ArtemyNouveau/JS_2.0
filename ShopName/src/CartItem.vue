1066231370213c08a222a145bbac44a45c9c41d2
ArtemyNouveau <47831303+ArtemyNouveau@users.noreply.github.com>
2019-07-04 18:46:01 +0300

    Add files via upload

---
+++ ShopName/src/CartItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <div class="col-md-4">{{product.product_name}}</div>
+        <div class="col-md-4">{{product.quantity}} шт</div>
+        <div class="col-md-4">{{product.price}}$</div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            product: {
+                type: Object,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .row{
+        border-bottom: 1px solid #dee2e6;
+    }
+
+    .row:last-child{
+        border-bottom: none;
+    }
+</style>
---
+++ ShopName/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+const vue = new Vue({
+  el: '#app',
+  data:{
+  },
+  render: h => h(App)
+});
